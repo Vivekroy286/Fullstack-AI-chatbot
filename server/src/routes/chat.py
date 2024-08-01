d0ccd3db85c0d21c52bdb09ad1c1e85c61c0deaa
Vivekroy286 <286royv@gmail.com>
2024-08-01 23:11:27 +0530

    update in schema

--- server/src/routes/chat.py
+++ server/src/routes/chat.py
@@ -1,34 +1,79 @@
-from fastapi import APIRouter, FastAPI, WebSocket,  Request, BackgroundTasks, HTTPException
+import os
+from fastapi import APIRouter, FastAPI, WebSocket, WebSocketDisconnect, Request, Depends
 import uuid
+from ..socket.connection import ConnectionManager
+from ..socket.utils import get_token
+import time
+from ..redis.producer import Producer
+from ..redis.config import Redis
+from ..schema.chat import Chat
+from rejson import Path
+
+chat = APIRouter()
+manager = ConnectionManager()
+redis = Redis()
+
 
 # @route   POST /token
-# @desc    Route generating chat token
+# @desc    Route to generate chat token
 # @access  Public
+
 
 @chat.post("/token")
 async def token_generator(name: str, request: Request):
+    token = str(uuid.uuid4())
 
     if name == "":
         raise HTTPException(status_code=400, detail={
             "loc": "name",  "msg": "Enter a valid name"})
 
-    token = str(uuid.uuid4())
+    # Create nee chat session
+    json_client = redis.create_rejson_connection()
+    chat_session = Chat(
+        token=token,
+        messages=[],
+        name=name
+    )
 
-    data = {"name": name, "token": token}
+    print(chat_session.dict())
 
-    return data
+    # Store chat session in redis JSON with the token as key
+    json_client.jsonset(str(token), Path.rootPath(), chat_session.dict())
 
-from ..socket.connection import ConnectionManager
+    # Set a timeout for redis data
+    redis_client = await redis.create_connection()
+    await redis_client.expire(str(token), 3600)
 
-manager = ConnectionManager()
+    return chat_session.dict()
+
+
+# @route   POST /refresh_token
+# @desc    Route to refresh token
+# @access  Public
+
+
+@chat.post("/refresh_token")
+async def refresh_token(request: Request):
+    return None
+
+
+# @route   Websocket /chat
+# @desc    Socket for chat bot
+# @access  Public
 
 @chat.websocket("/chat")
-async def websocket_endpoint(websocket: WebSocket):
+async def websocket_endpoint(websocket: WebSocket, token: str = Depends(get_token)):
     await manager.connect(websocket)
+    redis_client = await redis.create_connection()
+    producer = Producer(redis_client)
+    json_client = redis.create_rejson_connection()
+
     try:
         while True:
             data = await websocket.receive_text()
-            print(data)
+            stream_data = {}
+            stream_data[token] = data
+            await producer.add_to_stream(stream_data, "message_channel")
             await manager.send_personal_message(f"Response: Simulating response from the GPT service", websocket)
 
     except WebSocketDisconnect:
