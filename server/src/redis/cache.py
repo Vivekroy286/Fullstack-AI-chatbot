4a34849a9baf98071c585d86ede96e8d05d4186c
Vivekroy286 <286royv@gmail.com>
2024-08-06 21:00:30 +0530

    updates

--- server/src/redis/cache.py
+++ server/src/redis/cache.py
@@ -9,3 +9,22 @@ class Cache:
             str(token), Path.rootPath())
 
         return data
+class Cache:
+    def __init__(self, json_client):
+        self.json_client = json_client
+
+    async def get_chat_history(self, token: str):
+        data = self.json_client.jsonget(
+            str(token), Path.rootPath())
+
+        return data
+    
+class Cache:
+    def __init__(self, json_client):
+        self.json_client = json_client
+
+    async def get_chat_history(self, token: str):
+        data = self.json_client.jsonget(
+            str(token), Path.rootPath())
+
+        return data
--- server/src/routes/chat.py
+++ server/src/routes/chat.py
@@ -80,4 +80,12 @@ async def websocket_endpoint(websocket: WebSocket, token: str = Depends(get_toke
         manager.disconnect(websocket)
         from ..redis.cache import Cache
 
-  
+class Cache:
+    def __init__(self, json_client):
+        self.json_client = json_client
+
+    async def get_chat_history(self, token: str):
+        data = self.json_client.jsonget(
+            str(token), Path.rootPath())
+
+        return data  
