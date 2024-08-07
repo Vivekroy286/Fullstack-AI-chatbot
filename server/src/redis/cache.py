07ba2203ed95c98fe88341c7c57bcc6a4c3ea4ae
Vivekroy286 <286royv@gmail.com>
2024-08-07 16:43:19 +0530

    correction in codes

--- server/src/redis/cache.py
+++ server/src/redis/cache.py
@@ -9,22 +9,5 @@ class Cache:
             str(token), Path.rootPath())
 
         return data
-class Cache:
-    def __init__(self, json_client):
-        self.json_client = json_client
 
-    async def get_chat_history(self, token: str):
-        data = self.json_client.jsonget(
-            str(token), Path.rootPath())
-
-        return data
     
-class Cache:
-    def __init__(self, json_client):
-        self.json_client = json_client
-
-    async def get_chat_history(self, token: str):
-        data = self.json_client.jsonget(
-            str(token), Path.rootPath())
-
-        return data
--- server/src/redis/stream.py
+++ server/src/redis/stream.py
@@ -14,12 +14,3 @@ class StreamConsumer:
         
     from .config import Redis
 
-class Cache:
-    def __init__(self, json_client):
-        self.json_client = json_client
-
-    async def get_chat_history(self, token: str):
-        data = self.json_client.jsonget(
-            str(token), Path.rootPath())
-
-        return data 
