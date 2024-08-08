382e3ca6631c31895c880e1e580c919efda19086
Vivekroy286 <286royv@gmail.com>
2024-08-08 23:18:03 +0530

    update

--- server/src/routes/chat.py
+++ server/src/routes/chat.py
@@ -80,3 +80,35 @@ async def websocket_endpoint(websocket: WebSocket, token: str = Depends(get_toke
         manager.disconnect(websocket)
         from ..redis.cache import Cache
 
+class StreamConsumer:
+    def __init__(self, redis_client):
+        self.redis_client = redis_client
+
+    async def consume_stream(self, count: int, block: int,  stream_channel):
+
+        response = await self.redis_client.xread(
+            streams={stream_channel:  '0-0'}, count=count, block=block)
+
+        return response
+
+    async def delete_message(self, stream_channel, message_id):
+        await self.redis_client.xdel(stream_channel, message_id)
+        
+    from .config import Redis
+    
+    
+class StreamConsumer:
+    def __init__(self, redis_client):
+        self.redis_client = redis_client
+
+    async def consume_stream(self, count: int, block: int,  stream_channel):
+
+        response = await self.redis_client.xread(
+            streams={stream_channel:  '0-0'}, count=count, block=block)
+
+        return response
+
+    async def delete_message(self, stream_channel, message_id):
+        await self.redis_client.xdel(stream_channel, message_id)
+        
+    from .config import Redis
