929c955b2a9af243510d470ec9af93227fb04a01
Vivekroy286 <286royv@gmail.com>
2024-08-20 23:16:08 +0530

    05

--- server/src/redis/stream.py
+++ server/src/redis/stream.py
@@ -14,6 +14,18 @@ class StreamConsumer:
         await self.redis_client.xdel(stream_channel, message_id)
         
     from .config import Redis
-    
+    class ConnectionManager:
+    def __init__(self):
+        self.active_connections: List[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
     
 
